17dd1d9febbfafe7fc7e40dd23920ed0aab57615
Luque Montenegro <jose.luque-montenegro@hpe.com>
2018-04-04 09:22:07 +0200

    add css

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
@@ -91,6 +91,9 @@ app.get('/pagecount', function (req, res) {
     res.send('{ pageCount: -1 }');
   }
 });
+
+app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname + '/js'));
 
 // error handling
 app.use(function(err, req, res, next){
